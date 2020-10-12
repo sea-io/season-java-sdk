d07aed61bf36c5bc45a1f537dc1a145fc0c77371
tonyrodjohnson <69615421+tonyrodjohnson@users.noreply.github.com>
2020-10-12 14:24:40 +0800

    Create AddressFormatException.java

---
+++ src/main/java/net/season/core/crypto/AddressFormatException.java
@@ -0,0 +1,11 @@
+package net.season.core.crypto;
+
+public class AddressFormatException extends IllegalArgumentException {
+    public AddressFormatException() {
+        super();
+    }
+
+    public AddressFormatException(String message) {
+        super(message);
+    }
+}
